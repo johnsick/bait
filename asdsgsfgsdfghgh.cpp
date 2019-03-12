6ea2fd1fd932c3c7694f59e045e9f92d1540a2d3
johnsick <tomber2002@wp.pl>
2019-03-12 12:31:27 +0100

    bait.txt

---
+++ asdsgsfgsdfghgh.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	char sign;
+	queue<int> que;
+	while (cin >> sign)
+	{
+		if (sign == '+')
+		{
+			cin >> number;
+			que.push(number);
+		}
+		else
+			if (sign == '-')
+				if (!que.empty())
+					que.pop();
+				else
+					cout << "LMAO EMPTY" << endl;
+			else
+				cout << "Bitch u dumb" << endl;
+	}
+	while (!que.empty())
+	{
+		cout << que.front() << " ";
+		que.pop();
+	}
+	return 0;
+}
+
+
